18d580239024c72759632d7f77f279b81a3971a0
pduartedev <patrick.d.pimenta@gmail.com>
2025-07-14 13:39:33 -0300

    Feat: implementacao parcial lab-4

---
+++ frames-smalltalk/esqueleto-lab4/gramatica-6/ins/3.c
@@ -0,0 +1,14 @@
+double funcao(int a, double b, char c) {
+  int aa;
+  double bb;
+  int cc;
+  int dd;
+  cc = 10;
+  dd = 44;
+  {
+    int cc;
+    cc = 2;
+    dd = dd * cc;
+  }
+  aa = dd + cc;
+}
